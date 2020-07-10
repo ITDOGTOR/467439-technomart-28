4b94c684e8181e7b843ae88fcea253812875029d
Roman Kichatov <romkich16@gmail.com>
2020-07-10 18:55:58 +0300

    Оживлена форма «Напишите нам»

---
+++ js/write-us.js
@@ -0,0 +1,32 @@
+'use strict';
+
+(function () {
+  const writeUsModalOpen = document.querySelector('.contacts__form-link');
+  const writeUsModal = document.querySelector('.modal-write-us');
+  const writeUsModalClose = writeUsModal.querySelector('.modal__close-button');
+  const writeUsModalForm = writeUsModal.querySelector('.modal-write-us__form');
+  const writeUsModalInputs = writeUsModal.querySelectorAll('.modal-write-us__input');
+
+  writeUsModalOpen.addEventListener('click', function (evt) {
+    evt.preventDefault();
+    writeUsModal.classList.add('modal-show');
+  });
+
+  writeUsModalClose.addEventListener('click', function (evt) {
+    evt.preventDefault();
+    writeUsModal.classList.remove('modal-show');
+    writeUsModal.classList.remove('modal-error');
+  });
+
+  writeUsModalForm.addEventListener('submit', function (evt) {
+    for (let writeUsModalInput of writeUsModalInputs) {
+      if (!writeUsModalInput.value) {
+        evt.preventDefault();
+        writeUsModal.classList.remove('modal-error');
+        writeUsModal.offsetWidth; // хак для отработки анимации
+        writeUsModal.classList.add('modal-error');
+        break;
+      }
+    }
+  });
+})();
